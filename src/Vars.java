3a1dcaafaa940400ce840cd4b4b8599fb214be31
Mikhail Buzmakov <mbuzmak@mail.ru>
2026-09-16 14:33:02 +0300

    Решение Практической_задачи_1.2.6

---
+++ src/Vars.java
@@ -0,0 +1,12 @@
+public class Vars {
+    public static void main(String[] args) {
+        byte varByte = 127;
+        short varShort = 32767;
+        int varInt = 2_147_483_647;
+        long varLong = 9_223_372_036_854_775_807L;
+        float varFloat = 32.25435f;
+        double varDouble = 654.2346546432523;
+        boolean varBoolean = true;
+        char varChar = 'E';
+    }
+}
