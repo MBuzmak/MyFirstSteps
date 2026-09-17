5a3a7c1270a4a3463aaaa850cf725f43068a2dce
Mikhail Buzmakov <mbuzmak@mail.ru>
2026-09-17 13:01:31 +0300

    Решение Практической_задачи_1.4.3

--- src/NumberOfDay.java
+++ src/NumberOfDay.java
@@ -1,16 +1,6 @@
 public class NumberOfDay {
     public static boolean isWeekend(int weekDayNumber) {
-        if (weekDayNumber == 1) {
-            return (false);
-        } else if (weekDayNumber == 2) {
-            return (false);
-        } else if (weekDayNumber == 3) {
-            return (false);
-        } else if (weekDayNumber == 4) {
-            return (false);
-        } else if (weekDayNumber == 5) {
-            return (false);
-        } else if (weekDayNumber == 6) {
+        if (weekDayNumber == 6) {
             return (true);
         } else if (weekDayNumber == 7) {
             return (true);
